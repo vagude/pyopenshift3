a6b5ff032b2a5b9f1f3cba68a6ef0ffbb4493560
vamgude <vamgude@10.155.233.71>
2018-01-03 20:40:42 -0800

    added mongodb

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+    
+@application.route("/save/mongo")
+def saveInMongo():
+	host = os.getenv('MONGODB_SERVICE_HOST')
+	port = os.getenv('MONGODB_SERVICE_PORT')
+	username = os.getenv('MONGO_USERNAME')
+	password = os.getenv('MONGO_PASSWORD')
+	db = os.getenv('MONGO_SCHEMA')
+    return "{} - {} - {} - {} - {}".format(host, port, username, password, db)
 
 if __name__ == "__main__":
     application.run()
